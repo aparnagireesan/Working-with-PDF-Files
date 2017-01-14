4dc255e51cfdae79a6c99c75b347fe210d259509
aparnagireesan <aparnagireesan@gmail.com>
2017-01-14 15:03:25 +0530

    commented

--- Extract_PDF.py
+++ Extract_PDF.py
@@ -1,14 +1,20 @@
+# importing the required modules
 import PyPDF2
 
-pdfFileObj = open('example.pdf', 'rb')
+#Creating a pdf file object
+pdfFileObj = open('python_tutorial.pdf', 'rb')
 
+#creating a pdf reader object
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
+#printing the number of pdf pages
 print(pdfReader.numPages)
 
-pageObj = pdfReader.getPage(0)
+#creating a page object
+pageObj = pdfReader.getPage(1)
 
+#exrtacting the page from the specified page
 print(pageObj.extractText())
 
+#closing the pdf file object
 pdfFileObj.close()
-
